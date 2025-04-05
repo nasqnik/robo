749204c0de2222bcf3ddf765998b0834b73023dd
Ana <nasqnik@gmail.com>
2025-04-05 10:12:54 +0000

    section 48

--- src/bumperbot_description/launch/gazebo.launch.py
+++ src/bumperbot_description/launch/gazebo.launch.py
@@ -10,6 +10,8 @@ from launch.substitutions import Command, LaunchConfiguration
 
 def generate_launch_description():
     bumperbot_description_dir = get_package_share_directory("bumperbot_description")
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "humble" else "False"
 
     model_arg = DeclareLaunchArgument(
         name="model",
@@ -17,7 +19,12 @@ def generate_launch_description():
         description="Absolute path to robot URDF file"
     )
     
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]), value_type=str)
+    robot_description = ParameterValue(Command([
+        "xacro ", 
+        LaunchConfiguration("model"),
+        " is_ignition:=", is_ignition
+        ]),
+        value_type=str)
 
     robot_state_publisher = Node(
         package="robot_state_publisher",
