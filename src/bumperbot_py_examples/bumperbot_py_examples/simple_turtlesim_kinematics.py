e10d1d7cf61a62a1e505922a5f910e987af3015a
Ana <nasqnik@gmail.com>
2025-04-06 15:16:30 +0000

    61 - rotation matrix

--- src/bumperbot_py_examples/bumperbot_py_examples/simple_turtlesim_kinematics.py
+++ src/bumperbot_py_examples/bumperbot_py_examples/simple_turtlesim_kinematics.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
+import math
 
 class SimpleTurtlesimKinematics(Node):
     def __init__(self):
@@ -21,10 +22,19 @@ class SimpleTurtlesimKinematics(Node):
         Tx = self.last_turtle2_pose_.x - self.last_turtle1_pose_.x
         Ty = self.last_turtle2_pose_.y - self.last_turtle1_pose_.y
 
+        theta_rad = self.last_turtle2_pose_.theta - self.last_turtle1_pose_.theta
+        theta_deg = 180 * theta_rad / 3.14
+
         self.get_logger().info("""\n
                     Translation Vector between turtle1 -> turtle2 \n
                     Tx: %f \n
-                    Ty: %f \n""" % (Tx, Ty))
+                    Ty: %f \n
+                    Rotation Matrix between turtle1 -> turtle2\n
+                    theta(rad): %f \n
+                    theta(deg): %f \n
+                    |R11        R12| : | |%f        %f|\n
+                    |R21        R22| : | |%f        %f|\n""" % (Tx, Ty, theta_rad, theta_deg, math.cos(theta_rad), 
+                                                                -math.sin(theta_rad), math.sin(theta_rad), math.cos(theta_rad)))
 
 
 def main():
